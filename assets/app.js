109528bd7741d833220f3b1e32e1152e163a6df2
bee25141 <bee25141@gmail.com>
2019-04-17 21:31:24 -0500

    able to load gifs on click with new buttons, need to get rating to show and start/stop

--- assets/app.js
+++ assets/app.js
@@ -8,8 +8,9 @@ for (i=0; i<topics.length; i++){
 function buttonCreate(buttonText){
         var button = $("<button>");
         button.text(buttonText);
-        debugger;
+        // debugger;
         button.attr("value", buttonText);
+        button.addClass("button");
         $("#buttons-view").append(button);
 };
 //This function creates new buttons with the user's input
@@ -19,9 +20,9 @@ $(".submit").on("click", function(){
     buttonCreate(newButton);
 });
 
-$("button").on("click", function(){
+$(document).on("click", ".button", function(){
 var queryInput = $(this).val();
-debugger;
+// debugger;
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + queryInput + "&api_key=VYfqZGOrTXoZ7ECrxfyLv3KRkeogYqXu&limit=10";
 $.ajax({
     url: queryURL,
@@ -30,19 +31,31 @@ $.ajax({
 
   var results = response.data;
   for (i=0; i<results.length; i++){
-      console.log(results);
+      // console.log(results[i].rating);
       var gifDiv = $("<div>");
-      var rating = $("<p>" + results[i].images.rating);
+      // debugger;
+      var rating = $("<p>" + results[i].rating);
       var gifImage = $("<img>").attr("src", results[i].images.fixed_height.url);
+      console.log(results[i].images.fixed_height);
         // debugger;
       $("gifDiv").append(gifImage + rating);
+      $("gifDiv").addClass("gif");
       $("#image-container").prepend(gifImage);
+
   }
 });
 
-
-
-
+});
+$(".gif").on("click", function() {
+  var state = $(this).attr("data-state");
+ 
+  if (state === "still") {
+    $(this).attr("src", $(this).attr(results[i].images.fixed_height.url));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr(results[i].images.fixed_height_still.url));
+    $(this).attr("data-state", "still");
+  }
 });
 
 });
