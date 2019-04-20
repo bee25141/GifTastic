ac5c7f1668e0f39e242679c2a8db6bc1f9838331
bee25141 <bee25141@gmail.com>
2019-04-20 16:57:29 -0500

    refactored code

--- assets/app.js
+++ assets/app.js
@@ -8,38 +8,30 @@ $(document).ready(function () {
   function buttonCreate(buttonText) {
     var button = $("<button>");
     button.text(buttonText);
-    // debugger;
     button.attr("value", buttonText);
     button.addClass("button");
     $("#buttons-view").append(button);
   };
   //This function creates new buttons with the user's input
   $(".submit").on("click", function () {
-    console.log("click");
     var newButton = $("#newInput").val();
     buttonCreate(newButton);
   });
-
+  /*This function triggers the api call when the buttons are clicked, and displays
+  the gifs and gif ratings*/
   $(document).on("click", ".button", function () {
     var queryInput = $(this).val();
-    // debugger;
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + queryInput + "&api_key=VYfqZGOrTXoZ7ECrxfyLv3KRkeogYqXu&limit=10&rating=pg";
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function (response) {
-
       var results = response.data;
       for (i = 0; i < results.length; i++) {
-        // console.log(results[i].rating);
         var gifDiv = $("<div>");
-
         var rating = results[i].rating;
-        // debugger;
         var gifRating = $("<p>");
-        // console.log(results[i].rating);
         gifRating.text("Rating: "+ rating);
-        console.log(gifRating);
         var gifImage = $("<img>").attr("src", results[i].images.fixed_height.url);
         gifImage.attr("data-animate", results[i].images.fixed_height.url);
         gifImage.attr("data-still", results[i].images.fixed_height_still.url);
@@ -47,19 +39,15 @@ $(document).ready(function () {
         gifImage.addClass("gif");
         
         gifDiv.append(gifRating);
-        // debugger;
         gifDiv.append(gifImage);
         gifDiv.addClass("gif");
         $("#image-container").prepend(gifDiv);
       }
     });
-
-
   });
-
+  //This function changes the url source and data state to stop and play the gifs
   $(document).on("click", ".gif", function () {
     var state = $(this).attr("data-state");
-
     if (state === "still") {
       $(this).attr("src", $(this).attr("data-animate"));
       $(this).attr("data-state", "animate");
@@ -67,8 +55,5 @@ $(document).ready(function () {
       $(this).attr("src", $(this).attr("data-still"));
       $(this).attr("data-state", "still");
     }
-
-
   });
-
 });
